9c207b69a2f2f3b114d457b1684e74d978d184a2
terukazu inoue <terukazu.inoue@codianz.com>
2023-07-25 20:14:20 +0900

    Added a function to parse with a single `TLV`

--- src/extension.ts
+++ src/extension.ts
@@ -12,6 +12,14 @@ class RapduDCP implements vscode.TextDocumentContentProvider {
 	}
 }
 
+class TlvuDCP implements vscode.TextDocumentContentProvider {
+	onDidChange?: vscode.Event<vscode.Uri> | undefined;
+	provideTextDocumentContent(uri: vscode.Uri, token: vscode.CancellationToken): vscode.ProviderResult<string> {
+		const tlv = emv.tlv.create_from_data_chunk(emv.data_chunk.create_from_hex_string(uri.query));
+		return tlv.to_string();
+	}
+}
+
 async function setupEmvTags() {
 	emv.EmvTags.Instance.restoreDefault();
 	vscode.workspace.workspaceFolders?.forEach(async dir => {
@@ -29,16 +37,13 @@ async function setupEmvTags() {
 	});
 }
 
-export async function activate(context: vscode.ExtensionContext) {
-	await setupEmvTags();
-
-	const SCHEME = "emv-tools";
+function setupAnalyzeRAPDU(context: vscode.ExtensionContext) {
+	const SCHEME = "emv-tools-analyze-rapdu";
 	let counter = 1;
 
 	context.subscriptions.push(vscode.workspace.registerTextDocumentContentProvider(SCHEME, new RapduDCP()));
 
 	const disposable = vscode.commands.registerCommand('emv-tools.analyze_rapdu', async function() {
-		// Get the active text editor
 		const editor = vscode.window.activeTextEditor;
 		if (editor) {
 			const document = editor.document;
@@ -54,4 +59,34 @@ export async function activate(context: vscode.ExtensionContext) {
 	});
 
 	context.subscriptions.push(disposable);
+}
+
+function setupAnalyzeTLV(context: vscode.ExtensionContext) {
+	const SCHEME = "emv-tools-analyze-tlv";
+	let counter = 1;
+
+	context.subscriptions.push(vscode.workspace.registerTextDocumentContentProvider(SCHEME, new TlvuDCP()));
+
+	const disposable = vscode.commands.registerCommand('emv-tools.analyze_tlv', async function() {
+		const editor = vscode.window.activeTextEditor;
+		if (editor) {
+			const document = editor.document;
+			const selection = editor.selection;
+
+			// Get the word within the selection
+			const word = document.getText(selection);
+			const uri = vscode.Uri.parse(`${SCHEME}:tlv-${counter}?${word}`);
+			counter++;
+			const doc = await vscode.workspace.openTextDocument(uri);
+			await vscode.window.showTextDocument(doc, { preview: false });
+		}
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+export async function activate(context: vscode.ExtensionContext) {
+	await setupEmvTags();
+	setupAnalyzeRAPDU(context);
+	setupAnalyzeTLV(context);
 }
